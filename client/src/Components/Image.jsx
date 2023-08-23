a21686e6ed1c13852427360bce87fb66d4cef97e
Ayan <ayan.altynbekuly@gmail.com>
2023-08-23 16:40:10 +0600

    image fix

--- client/src/Components/Image.jsx
+++ client/src/Components/Image.jsx
@@ -2,6 +2,6 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("https://")
       ? src
-      : "http://localhost:8080/upload/" + src;
+      : "http://localhost:8080/api/upload/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
